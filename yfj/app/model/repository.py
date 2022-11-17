a54368f9307827b4d8ac074bc20d7053b3956997
Robert Ta <robert.ta@soxes.ch>
2022-11-17 12:17:52 +0700

    (assignment) fix transaction issue

--- yfj/app/model/repository.py
+++ yfj/app/model/repository.py
@@ -2,10 +2,10 @@
 
 from abc import ABC, abstractmethod
 
-from sqlalchemy import desc
+from sqlalchemy import desc, and_
 
+from app.base.base_model import BaseModel
 from app.database import db
-from .models import Model
 
 
 class Repository(ABC):
@@ -18,7 +18,7 @@ class Repository(ABC):
         self.__model_class = model_class
         self.session = db.session
 
-    def get(self, model_id: int) -> Model:
+    def get(self, model_id: any) -> BaseModel:
         """Retrieve a model register from database by its id.
         Parameters:
            model_id (int): Id of the model to be retrieved.
@@ -26,7 +26,8 @@ class Repository(ABC):
            Model: a model object.
         """
 
-        return self.session.query(self.__model_class).filter_by(id=model_id).first()
+        return self.session.query(self.__model_class).filter(
+            and_(self.__model_class.id == model_id, self.__model_class.deleted_at == None)).first()
 
     def get_all(self) -> list:
         """Retrieves a list of all elements in the database.
@@ -34,9 +35,10 @@ class Repository(ABC):
            list: a list of model objects.
         """
 
-        return self.session.query(self.__model_class).order_by(desc(self.__model_class.id))
+        return self.session.query(self.__model_class).filter(
+            and_(self.__model_class.deleted_at == None)).order_by(desc(self.__model_class.id))
 
-    def save(self, model: Model) -> None:
+    def save(self, model: BaseModel) -> None:
         """Saves a model in the database.
         Parameters:
            model (Model): A model object.
@@ -45,7 +47,7 @@ class Repository(ABC):
         self.session.add(model)
         self.session.commit()
 
-    def update(self, model: Model) -> None:
+    def update(self, model: BaseModel) -> None:
         """Update a existent model register in the database.
         Parameters:
            model (Model): A model object.
@@ -53,21 +55,20 @@ class Repository(ABC):
 
         self.session.commit()
 
-    def delete(self, model: Model) -> int:
+    def delete(self, model: BaseModel) -> bool:
         """Delete a existent model register in the database.
         Parameters:
            model (Model): A model object.
         Returns:
            int: the a model id that was deleted.
         """
-
-        deleted = self.session.delete(model)
+        model.deleted_at = db.func.now()
         self.session.commit()
 
-        return deleted
+        return True
 
     @abstractmethod
-    def is_invalid(self, model: Model) -> list:
+    def is_invalid(self, model: BaseModel) -> list:
         """Checks if a given model object is valid.
         Parameters:
             model (Model): The model object.
